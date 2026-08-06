8cf62213aac5b5f14dafe2ebbab193602738311e
thaisqabe <thaisq.abe@gmail.com>
2026-08-06 08:40:04 -0300

    aula01 06-08-26

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import Base, engine, get_db
+from models import ProdutoDB
+from schemas import ProdutoCreate, ProdutoResponse
+
+Base.metadata.create_all(bind=engine) # cria as tabelas, se ainda não existirem
 
 app = FastAPI()
 
-@app.get('/')  #@ decorator
+@app.get('/produtos', response_model=list[ProdutoResponse])
+def listar_produtos(db: Session = Depends(get_db)):
+    return db.query(ProdutoDB).all()
 
-def raiz():
-    return {'mensagem': 'Minha primeira API em FastAPI!'}
 
-@app.get('/sobre')
-def sobre():
-    return {'mensagem': 'Página sobre o site'}
+@app.post('/produtos', response_model=ProdutoResponse, status_code=201)
+def criar_produto(produto: ProdutoCreate, db: Session = Depends(get_db)):
+    novo_produto = ProdutoDB(**produto.dict())
+    db.add(novo_produto)
+    db.commit()
+    db.refresh(novo_produto)
+    return novo_produto
+
---
+++ schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+class ProdutoBase(BaseModel):
+    nome: str
+    preco: float
+    quantidade: int
+
+
+class ProdutoCreate(ProdutoBase):
+    pass
+
+
+class ProdutoResponse(ProdutoBase):
+    id: int
+
+
+class Config:
+    from_attributes = True
